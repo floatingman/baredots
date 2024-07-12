4302644eb723694446d3a9509ba99aed45c9ef7f
Daniel Newman <dan@danlovesprogramming.com>
2024-07-11 21:10:35 -0500

    Changed wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,28 @@
 local wezterm = require("wezterm")
+
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Catppuccin Mocha'
+  else
+    return 'Catppuccin Latte'
+  end
+end
+
 return {
 	-- color_scheme = 'termnial.sexy',
-	color_scheme = "Catppuccin Mocha",
+	color_scheme = scheme_for_appearance(get_appearance()),
 	enable_tab_bar = false,
+	font = wezterm.font("JetBrains Mono", {weight = "Regular"}),
 	font_size = 16.0,
 	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
